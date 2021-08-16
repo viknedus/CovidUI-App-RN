f54631176e518cdc85dbd2f2c6edad57ceb42051
viknedus <eduvanvictor@yahoo.com>
2021-08-16 21:35:41 +0100

    cleanup

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,9 +1,66 @@
 import React from "react";
-import { View, Button, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  FlatList,
+  SectionList,
+  Image,
+} from "react-native";
 // local
 import { TextInput } from "react-native-paper";
 // dependencies
 import { SafeAreaView } from "react-native-safe-area-context";
+
+// section data
+const SECTIONS = {
+  title: "Made for you",
+  // horizontal: true,
+  data: [
+    {
+      key: "1",
+      text: "Item text 1",
+      uri: "https://picsum.photos/id/1/200",
+    },
+    {
+      key: "2",
+      text: "Item text 2",
+      uri: "https://picsum.photos/id/10/200",
+    },
+
+    {
+      key: "3",
+      text: "Item text 3",
+      uri: "https://picsum.photos/id/1002/200",
+    },
+    {
+      key: "4",
+      text: "Item text 4",
+      uri: "https://picsum.photos/id/1006/200",
+    },
+    {
+      key: "5",
+      text: "Item text 5",
+      uri: "https://picsum.photos/id/1008/200",
+    },
+  ],
+};
+
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={{
+          uri: item.uri,
+        }}
+        style={styles.itemPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+};
 
 function DetailsScreen({ navigation }) {
   return (
@@ -18,6 +75,26 @@ function DetailsScreen({ navigation }) {
         right={<TextInput.Icon name="arrow-right-box" />}
       />
       <Text>Symptoms</Text>
+      <SectionList
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+        stickySectionHeadersEnabled={false}
+        sections={SECTIONS}
+        renderSectionHeader={({ section }) => (
+          <>
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+            <FlatList
+              horizontal
+              data={section.data}
+              renderItem={({ item }) => <ListItem item={item} />}
+              showsHorizontalScrollIndicator={false}
+            />
+          </>
+        )}
+        renderItem={({ item, section }) => {
+          return null;
+          // return <ListItem item={item} />;
+        }}
+      />
 
       <Button
         title="Go to Details... again"
